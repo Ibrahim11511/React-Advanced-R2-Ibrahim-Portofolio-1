eb569348e2f55c69fe0ffd93ee9b3b4042430ef1
Ibrahim11511 <109541964+Ibrahim11511@users.noreply.github.com>
2024-07-13 00:04:22 +0300

    create side bar and make it responsive

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SideBar from "./Components/SideBar";
+import "@fontsource/playfair";
+import "@fontsource/quicksand";
+function App() {
+  return (
+    <>
+      <SideBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import profilePicture from "../Images/about-BgAkqdr2.jpg";
+import "./SideBar.css";
+export default function SiderBar() {
+  const [isActive, setActive] = useState(false);
+  function handelActive() {
+    setActive(!isActive);
+  }
+  return (
+    <aside className={isActive ? "active" : ""}>
+      <button className="clo-ope-aside-btn" onClick={handelActive}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div className="userImage">
+        <img src={profilePicture} alt="profile picture" />
+      </div>
+
+      <div className="info">
+        <span className="userName">
+          Jackson <br />
+          Ford
+        </span>
+        <p className="userJopTitle">
+          <a href="#">UI/UX/DESIGNER </a>
+          IN PHILIPPINES
+        </p>
+      </div>
+
+      <ul>
+        <li>
+          <a className="active" href="#">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="#">about</a>
+        </li>
+        <li>
+          <a href="#">skills</a>
+        </li>
+        <li>
+          <a href="#">EXPERIENCE</a>
+        </li>
+        <li>
+          <a href="#">work</a>
+        </li>
+      </ul>
+    </aside>
+  );
+}
